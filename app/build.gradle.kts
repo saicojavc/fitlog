180588a33f0b2ecaa25b50e6d9905331509fe275
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2026-02-14 02:11:37 -0500

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
